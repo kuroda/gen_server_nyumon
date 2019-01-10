e8b31bfdae9e3d675700752b415469f48307acd2
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-01-10 17:05:27 +0900

    test_app: simplify mix.exs

--- test_app/mix.exs
+++ test_app/mix.exs
@@ -7,17 +7,12 @@ defmodule TestProject do
       version: "0.0.1",
       elixir: "~> 1.7",
       elixirc_paths: ["lib"],
-      compilers: Mix.compilers(),
-      start_permanent: Mix.env() == :prod,
-      aliases: [],
-      deps: []
     ]
   end
 
   def application do
     [
-      mod: {TestApp, []},
-      applications: [:logger]
+      mod: {TestApp, []}
     ]
   end
 end
